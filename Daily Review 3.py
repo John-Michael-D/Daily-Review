d3dc42e41a3bc5213ec0a6060fd39b12b5014409
John-Michael-D <johnm.dantes@yahoo.com>
2025-04-11 14:28:39 -0400

    Add files via upload

---
+++ Daily Review 3.py
@@ -0,0 +1,28 @@
+print(f"\nProblem Alpha")
+
+sum = 0
+i = 0
+
+while i < 3:
+    sum = sum + i
+    print(sum)
+    print("")
+    i = i + 1
+
+print(f"\nProblem Bravo")
+
+i = 0
+
+while i < 8:
+    print(i)
+    print("")
+    i = i + 2
+
+print(f"\nProblem Charlie")
+
+i = 5
+
+while i > 0:
+    print(i)
+    print("")
+    i = i - 1
