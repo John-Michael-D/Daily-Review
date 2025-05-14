f42933df2b67ad4acc5a913488d5f082b292ae6f
John-Michael-D <johnm.dantes@yahoo.com>
2025-05-14 12:12:33 -0400

    Add files via upload

---
+++ Daily Review 16.py
@@ -0,0 +1,17 @@
+import turtle
+
+franklin = turtle.Turtle()
+myScreen = turtle.Screen()
+franklin.shape("turtle")
+franklin.color("blue")
+franklin.forward(100)
+franklin.right(90)
+franklin.forward(100)
+franklin.right(90)
+franklin.forward(100)
+franklin.right(90)
+franklin.forward(100)
+franklin.right(90)
+franklin.forward(100)
+myScreen.canvheight
+myScreen.exitonclick()
